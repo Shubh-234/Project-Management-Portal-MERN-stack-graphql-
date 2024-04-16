5e9fa0afafdb828de98f4913ecbabe45ee45267c
Shubhankar Bhanot <shubhankarbhanot@gmail.com>
2024-04-16 14:42:48 +0530

    Project and Client queries

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require('dotenv').config(); 
+
+const {graphqlHTTP} = require('express-graphql');
+const colors = require('colors');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+//connecting to the database
+connectDB();
+app.use('/graphql',graphqlHTTP({
+  schema,
+  graphiql:process.env.NODE_ENV === 'development',
+  //meaninng the graphiql will only be enable if the enviroment is development
+}));
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
